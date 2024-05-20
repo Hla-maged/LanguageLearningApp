890f9c415c78626fc189665156283af74295ab07
Hla Maged <161922890+Hla-maged@users.noreply.github.com>
2024-05-20 14:44:28 +0300

    Update weather_service.dart

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,21 +1,27 @@
-import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:project/models/weather_model.dart';
 
 class WeatherService {
+  final Dio dio = Dio();
   String baseURL = 'http://api.weatherapi.com/v1';
-      String APIkey = '015033bf6c9c4e7e9f6203335242503';
-  Future<WeatherModel?> getWeather({required String CityName}) async {
-    WeatherModel? weather;
-  
-      Uri url =
-          Uri.parse('$baseURL/forecast.json?key=$APIkey&q=$CityName&days=1');
+  String APIkey = '015033bf6c9c4e7e9f6203335242503';
 
-      http.Response response = await http.get(url);
-      Map<String, dynamic> data = jsonDecode(response.body);
-      weather = WeatherModel.fromJson(data);
-  
-    return weather;
+  WeatherService(Dio dio);
+  Future<WeatherModel> getWeather({required String CityName}) async {
+    WeatherModel? weather;
+    try {
+      Response response = await dio.get(
+          '$baseURL/forecast.json?key=$APIkey&q=$CityName&days=1'); //response
+      weather = WeatherModel.fromJson(
+          response.data); //take data and pass it to weathermodel
+      return weather;
+    } on DioException catch (e) {
+      final String error =
+          e.response?.data['error']['message'] ?? 'opps! there was an error';
+      throw Exception(error);
+    } catch (e) {
+      throw Exception('there was an error');
+    }
   }
 }
