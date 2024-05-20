156b71837a012656077788c7da743a63173b0d30
Hla Maged <161922890+Hla-maged@users.noreply.github.com>
2024-05-20 14:48:23 +0300

    Update search_page.dart

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,79 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:project/models/weather_model.dart';
-// import 'package:project/provider/weatherProvider.dart';
-// import 'package:project/services/weather_service.dart';
-// import 'package:provider/provider.dart';
 
-// class SearchPage extends StatelessWidget {
-//   String? CityName;
-//   SearchPage({required this.updateUi});
-//   VoidCallback? updateUi;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text(
-//             'Search a City',
-//             style: TextStyle(color: Colors.white),
-//           ),
-//           backgroundColor: Colors.blue,
-//         ),
-//         body: Center(
-//           child: Padding(
-//             padding: const EdgeInsets.symmetric(horizontal: 14),
-//             child: TextField(
-//               onChanged: (value) {
-//                 CityName = value;
-//               },
-//               onSubmitted: (value) async {
-//                 CityName = value;
-//                 Weather Service = Weather();
-//                 WeatherModel? weather =
-//                     await Service.getWeather(CityName: CityName!);
-
-//                 Provider.of<WeatherProvider>(context, listen: false).DATA =
-//                     weather;
-//                 Provider.of<WeatherProvider>(context, listen: false).CityName =
-//                     CityName;
-//                 Navigator.pop(context);
-//               },
-//               decoration: InputDecoration(
-//                 label: Text('Search'),
-//                 hintText: 'Enter City Name',
-//                 border: OutlineInputBorder(),
-//                 contentPadding:
-//                     EdgeInsets.symmetric(vertical: 32, horizontal: 24),
-//                 suffixIcon: GestureDetector(
-//                   onTap: () async {
-//                     Weather Service = Weather();
-//                     WeatherModel? Wmodel =
-//                         await Service.getWeather(CityName: CityName!);
-
-//                     Provider.of<WeatherProvider>(context, listen: false).DATA =
-//                         Wmodel;
-//                     Provider.of<WeatherProvider>(context, listen: false)
-//                         .CityName = CityName;
-//                     Navigator.pop(context);
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ),
-//         )
-//         );
-//   }
-// }
-
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:project/models/weather_model.dart';
-import 'package:project/provider/weatherProvider.dart';
 import 'package:project/services/weather_service.dart';
-import 'package:provider/provider.dart';
 
 class SearchPage extends StatelessWidget {
- String? CityName;
- SearchPage({this.updateUi});
+  String? CityName;
+  SearchPage({this.updateUi});
   VoidCallback? updateUi;
   @override
   Widget build(BuildContext context) {
@@ -90,17 +25,12 @@ class SearchPage extends StatelessWidget {
               CityName = data;
             },
             onSubmitted: (data) async {
-              CityName = data;
+              var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
+              getWeatherCubit.getWeather(CityName:data);
+              // WeatherService service = WeatherService(Dio());
 
-              WeatherService service = WeatherService();
-
-              WeatherModel? weather =
-                  await service.getWeather(CityName: CityName!);
-
-              Provider.of<WeatherProvider>(context, listen: false).DATA =
-                  weather;
-                           Provider.of<WeatherProvider>(context, listen: false).CityName =
-                               CityName;
+              // WeatherModel? weather =
+              //     await service.getWeather(CityName: CityName!);
 
               Navigator.pop(context);
             },
@@ -110,16 +40,17 @@ class SearchPage extends StatelessWidget {
               label: Text('search'),
               suffixIcon: GestureDetector(
                   onTap: () async {
-                    WeatherService service = WeatherService();
+                    WeatherService service = WeatherService(Dio());
 
-                    WeatherModel? weather =
-                        await service.getWeather(CityName: CityName!);
-
-                    Provider.of<WeatherProvider>(context, listen: false).DATA =
-                        weather;
-                                  Provider.of<WeatherProvider>(context, listen: false)
-                                      .CityName = CityName;
-
+                    WeatherModel? weather = await service.getWeather(
+                        CityName:
+                            CityName!); //in case we want to use this value in another view we can't use setState(),
+                    // because when we convert the class to statefulwidget to update ui we can oly use setstate in this class not in another page,
+                    // so we want to use another state management=> Bloc -- to control the state, seperate ui from logic
+                    //- Bloc consists of "logic" => what will happen,,"states"=> to talk with the part that will change,"events"=>the action taken by user that will active the logic
+                    // but we will use "Cubit" instead of bloc - we won't deal with events
+                    // cubit=> handle the logic that happens and the ui that will change
+                    // the steps: create states >> create cubit >> create function >> provide cubit >> integrate cubit >> trigger cubit
                     Navigator.pop(context);
                   },
                   child: Icon(Icons.search)),
