de0a7a3e36aa08a9ac4e8486f4a9860f0e97c39d
Hla Maged <161922890+Hla-maged@users.noreply.github.com>
2024-05-20 14:43:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
-import 'package:project/models/weather_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project/cubits/get_weather_cubit/get_weather_cubit.dart';
+
 import 'package:project/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:project/pages/search_page.dart';
 import 'package:project/provider/weatherProvider.dart';
 import 'package:provider/provider.dart';
 
@@ -20,13 +18,23 @@ void main() {
 class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Provider.of<WeatherProvider>(context).DATA == null
-              ? Colors.blue
-              : Provider.of<WeatherProvider>(context).DATA!.getColor()),
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    // we use material app to provide cubit to both homView and searchView
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: Material()
+    );
+  }
+}
+
+class Material extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        theme: ThemeData(
+            primarySwatch: 
+                BlocProvider.of<GetWeatherCubit>(context).DATA?.getColor()),
+        debugShowCheckedModeBanner: false,
+        home: Home(),
     );
   }
 }
